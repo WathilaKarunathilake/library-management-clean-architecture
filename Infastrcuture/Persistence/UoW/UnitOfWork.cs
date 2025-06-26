459bafddd9ef21fe85f1e4b7ae54a5f2d874f172
Wathila Karunathilake <wathila.karunathilake@ascentic.se>
2025-06-26 09:23:41 +0530

    Added DI for UnitOfWork and added RollBack in UnitOfWork

--- Infastrcuture/Persistence/UoW/UnitOfWork.cs
+++ Infastrcuture/Persistence/UoW/UnitOfWork.cs
@@ -3,31 +3,56 @@ using LibraryManagementCleanArchitecture.Core.Application;
 using LibraryManagementCleanArchitecture.Domain.Entities;
 using LibraryManagementCleanArchitecture.Infastrucute.Persistence.Context;
 using LibraryManagementCleanArchitecture.Infastrucuture.Persistence.Context;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace LibraryManagementCleanArchitecture.Persistence.UoW
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private readonly AppDbContext _context;
+        private readonly AppDbContext context;
 
         public IRepository<Book> Books { get; }
         public IRepository<Member> Members { get; }
+        private IDbContextTransaction? currentTransaction;
 
-        public UnitOfWork(AppDbContext context)
+        public UnitOfWork(AppDbContext context, IRepository<Book> books, IRepository<Member> members)
         {
-            _context = context;
-            Members = new Repository<Member>(_context); 
-            Books = new Repository<Book>(_context);
+            this.context = context;
+            Books = books;
+            Members = members;
         }
 
         public async Task<int> SaveChangesAsync()
         {
-            return await _context.SaveChangesAsync();
+            try
+            {
+                currentTransaction = await context.Database.BeginTransactionAsync();
+                var result = await context.SaveChangesAsync();
+                await currentTransaction.CommitAsync();
+                return result;
+            }
+            catch (Exception)
+            {
+                if (currentTransaction == null)
+                {
+                    await currentTransaction.RollbackAsync();
+                }
+                throw;
+            }
+            finally
+            {
+
+                if (currentTransaction != null)
+                {
+                    await currentTransaction.DisposeAsync();
+                    currentTransaction = null;
+                }
+            }
         }
 
         public void Dispose()
         {
-            _context.Dispose();
+           context.Dispose();
         }
     }
 
